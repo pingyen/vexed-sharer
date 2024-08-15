885539e2aaff2c235c74dc6715c4bcbb101548e9
Ping-Yen Tsai <pingyen@cloudmosa.com>
2024-08-16 02:11:07 +0800

    Support term.ptt.cc.

--- sw.js
+++ sw.js
@@ -1,7 +1,22 @@
 chrome.action.onClicked.addListener(tab => {
   chrome.scripting.executeScript({
     target: { tabId: tab.id },
-    func: (url) => { location = 'https://m.facebook.com/sharer/sharer.php?u=' + encodeURIComponent(url); },
+    func: (url) => {
+      if (url === 'https://term.ptt.cc/') {
+        const anchor = document.querySelector('[href^="https://www.ptt.cc/bbs/"]');
+
+        anchor === null ?
+          alert('Cannot find the URL.') :
+          window.open(
+            'https://m.facebook.com/sharer/sharer.php?u=' + encodeURIComponent(anchor.href),
+            '_blank',
+            `width=100,height=100,top=${window.innerHeight - 100},left=${window.innerWidth - 100}`);
+
+        return;
+      }
+
+      location = 'https://m.facebook.com/sharer/sharer.php?u=' + encodeURIComponent(url);
+    },
     args: [tab.url]
   });
 });
