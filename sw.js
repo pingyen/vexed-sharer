7097b2c91536707634039bbadd575048198aaeb1
Ping-Yen Tsai <pingyen@cloudmosa.com>
2024-08-16 00:58:28 +0800

    Update Facebook Sharer opening method.

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 chrome.action.onClicked.addListener(tab => {
   chrome.scripting.executeScript({
     target: { tabId: tab.id },
-    func: (url) => { window.open('https://m.facebook.com/sharer/sharer.php?u=' + encodeURIComponent(url)) },
+    func: (url) => { location = 'https://m.facebook.com/sharer/sharer.php?u=' + encodeURIComponent(url); },
     args: [tab.url]
   });
 });
